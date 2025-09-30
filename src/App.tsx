528c8517e7e565e017e48f8465b82e263959c5c5
Johnny Willy <johnnywilly16@gmail.com>
2025-09-29 23:19:14 -0300

    ill continue tomorrow: tired

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-
   return (
     <>
-      <Heading />
-      <h1>Olá mundo!</h1>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
         nostrum, sit quas velit blanditiis cupiditate enim minima soluta modi
