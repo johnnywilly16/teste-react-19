7569640460244252fb441f585d163cf37c308aef
Johnny Willy <johnnywilly16@gmail.com>
2025-10-01 14:40:04 -0300

    estudo de estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-//Pascal Case
-//App
-//Header
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-        nostrum, sit quas velit blanditiis cupiditate enim minima soluta modi
-        voluptas quia mollitia, consequatur eius, eos nihil laborum veniam
-        voluptatem repellat?
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias animi
+            accusamus praesentium, quis molestiae eum iste tenetur beatae,
+            facilis, mollitia sequi natus ab tempore temporibus debitis! Earum
+            quae odit perspiciatis.
+          </section>
+        </div>
+      </div>
     </>
   );
 }
